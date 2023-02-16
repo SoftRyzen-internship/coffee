4834f556f9ac31c712c9bfdc6b3a2e36a6ad5dbd
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-02-16 12:48:19 +0200

    add-styles-mobile

--- src/assets/js/menu-categories.js
+++ src/assets/js/menu-categories.js
@@ -2,10 +2,10 @@ import $ from 'jquery';
 import 'slick-carousel';
 
 $('.category__slick-slider').slick({
-  slidesToShow: 2,
+  slidesToShow: 3,
   slidesToScroll: 1,
   mobileFirst: true,
-  arrows: false,
-  dots: true,
-  fade: true,
+  infinite: false,
+  arrows: true,
+  dots: false,
 });

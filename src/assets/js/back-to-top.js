4b43d15b3ec66a9e3f6c01b554eb7fffef92c167
Kretsul-Vitaliy <turistvital1988@gmail.com>
2023-02-19 16:03:07 +0200

    Revert "Revert "add-footer and add fonts woff woff2""
    This reverts commit f14c9b1ecbb6ce1653821c0720282b22bd8a2b07.

--- src/assets/js/back-to-top.js
+++ src/assets/js/back-to-top.js
@@ -1,15 +1,22 @@
 import $ from 'jquery';
 
-$(window).scroll(function () {
-  let scrolled = $(window).scrollTop();
-  console.log(scrolled);
-  if (scrolled > 300) {
-    $('#back-to-top').addClass('active');
-  } else {
-    $('#back-to-top').removeClass('active');
-  }
-});
+$(document).ready(function () {
+  let backToTop = $('#back-to-top');
 
-$('#back-to-top').click(function () {
-  $('body, html').animate({ scrollTop: 0 }, 1000);
+  if (backToTop.length) {
+    $(window).scroll(function () {
+      let scrolled = $(window).scrollTop();
+      console.log(scrolled);
+
+      if (scrolled > 300) {
+        backToTop.addClass('active');
+      } else {
+        backToTop.removeClass('active');
+      }
+    });
+
+    backToTop.click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 1000);
+    });
+  }
 });

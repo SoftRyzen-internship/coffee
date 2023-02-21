ad13ec9593fab4c8ed4112353ac741e8fdba55f6
Ihor Riabenko <100082550+iryabenkoff@users.noreply.github.com>
2023-02-21 17:52:32 +0200

    ME-3: btn clicked, window location to menu

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -12,9 +12,15 @@ const handleClick = () => {
     }
     btn.textContent = 'Close menu';
   } else {
+
+    if (window.matchMedia("(max-width: 1279px)").matches) {
+      window.location.href='#menu';
+    }
+
     for(let el of hiddenMenu) {
       el.style.display = 'none';
     }
+
     btn.textContent = 'View all menu';
   }
 };
